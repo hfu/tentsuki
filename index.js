ba30fc906acdf4b11da9556afdf8917b81bef634
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-11-22 12:31:24 -0500

    update

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const fs = require('fs')
 const es = require('event-stream')
 const tilebelt = require('@mapbox/tilebelt')
 const bbox = require('@turf/bbox').default
+const modify = require('./modify.js')
+
+// You may want to 'ulimit -n 65536'
 
 const streams = {}
 const streamWrite = (w3n, s) => {
@@ -24,20 +27,22 @@ const work = async (src, encoding) => {
       .pipe(es.mapSync(line => {
         s.pause()
         if (line.length === 0) return
-        const f = JSON.parse(line)
+        let f = JSON.parse(line)
         const b = bbox(f)
         const w = [[0, 1], [0, 3], [2, 1], [2, 3]].map(v => {
           const [x, y, z] = tilebelt.pointToTile(b[v[0]], b[v[1]], config.get('z'))
           return `${z}-${x}-${y}`
         }).filter((x, i, self) => self.indexOf(x) === i)
         for (const w3n of w) {
-          streamWrite(w3n,`${JSON.stringify(f)}\n`)
+          f = modify(f)
+          if (f) streamWrite(w3n,`${JSON.stringify(f)}\n`)
         }
         if (++count % 10000 === 0) status(count, w[0], src)
         s.resume()
       })
       .on('error', err => {
-        console.log(err)
+        console.error(err)
+        s.resume()
       })
       .on('end', () => {
         status(count, '-', src)
